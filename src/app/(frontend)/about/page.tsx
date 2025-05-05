e73de8673c74bd9ddd6d8d662d6096a0e013aba6
nwil850 <nwil850@aucklanduni.ac.nz>
2025-05-05 16:11:41 +1200

    Created pages and added footer

---
+++ src/app/(frontend)/about/page.tsx
@@ -0,0 +1,9 @@
+// app/gallery/page.tsx
+import { Suspense } from 'react'
+import { getEventsWithPhotos } from '@/libs/server'
+import Loading from './loading'
+import GalleryClient from '@/components/GalleryClient'
+
+export default function AboutPage() {
+  return <div>hello from the about page</div>
+}
