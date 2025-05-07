20dde4b4feebc3ddeea9499720b45579f3c61a63
alannadee <139197719+alannadee@users.noreply.github.com>
2025-05-07 15:34:14 +1200

    sponsors-page v1
    .csv file is parsed, images & links are referenced. some grid formatting

--- src/app/(frontend)/sponsors/page.tsx
+++ src/app/(frontend)/sponsors/page.tsx
@@ -1,13 +1,50 @@
 // app/gallery/page.tsx
-import { Suspense } from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 import { getEventsWithPhotos } from '@/libs/server'
 import Loading from './loading'
+import fs from 'node:fs'
+import Image from 'next/image'
 import GalleryClient from '@/components/GalleryClient'
+import Papa, { ParseResult } from 'papaparse'
+import { number } from 'node_modules/payload/dist/fields/validations'
+
+type Sponsor = {
+  id: number
+  image: string
+  link: string
+}
+
+const file = fs.readFileSync('./public/sponsors/sponsors_test.csv', 'utf8')
+
+const parsed = Papa.parse<Sponsor>(file, {
+  delimiter: ',',
+  dynamicTyping: true,
+  header: true,
+  skipEmptyLines: true,
+})
+
+const sponsors: Sponsor[] = parsed.data as Sponsor[]
 
 export default function AboutPage() {
   return (
     <div>
       <div className="bg-tansa-blue h-[400px]"></div>
+      <div className="mt-15 mx-auto w-[50vw]">
+        <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols6 gap-5">
+          {sponsors.map((sponsor, index) => (
+            <div key={index} className="flex items-center gap-4">
+              <a href={sponsor.link}>
+                <Image
+                  src={`/sponsors/images/${sponsor.image}`}
+                  alt={sponsor.image}
+                  width={100}
+                  height={100}
+                />
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
       <div className="bg-tansa-cream h-[600px]"></div>
     </div>
   )
