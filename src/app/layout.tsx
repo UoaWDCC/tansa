f2a9aef4974409b5b4e337139808f9728ad3c876
nwil850 <nwil850@aucklanduni.ac.nz>
2025-05-14 01:35:26 +1200

    feat: 404 page created

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './(frontend)/styles.css'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+
+export const metadata = {
+  title: 'WDCC - Tansa',
+}
+
+export default async function RootLayout(props: { children: React.ReactNode }) {
+  const { children } = props
+
+  return (
+    <html className="bg-tansa-blue" lang="en">
+      <body className={`antialiased bg-tansa-blue`} suppressHydrationWarning>
+        <main>
+          <div className="bg-tansa-cream min-h-screen flex flex-col">
+            <Header />
+            {children}
+            <Footer />
+          </div>
+        </main>
+      </body>
+    </html>
+  )
+}
