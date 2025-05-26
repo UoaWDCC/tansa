e2b0de76a73b56bf2963bae0fd50905e9fc01aa2
deasylie <dlie042@aucklanduni.ac.nz>
2025-05-26 21:40:50 +1200

    add new kansas bold font to styles.css

--- src/app/(frontend)/about/page.tsx
+++ src/app/(frontend)/about/page.tsx
@@ -2,16 +2,14 @@
 import { Suspense } from 'react'
 import { getEventsWithPhotos } from '@/libs/server'
 import Loading from './loading'
-// import GalleryClient from '@/components/GalleryClient'
-import bear from '/bears/lying_on_stomach.png'
 
 export default function AboutPage() {
   return (
     <div>
       <div className="bg-tansa-blue h-[400px] flex flex-col justify-end relative overflow-hidden">
         <div className="container mx-20 px-20 mt-20 mb-10 relative z-10">
-          <h1 className="text-6xl font-bold text-tansa-cream">Meet our</h1>
-          <h1 className="text-8xl font-bold text-tansa-cream">Team!</h1>
+          <h1 className="text-6xl text-tansa-cream font-newkansas">Meet our</h1>
+          <h1 className="text-8xl text-tansa-cream font-newkansas">Team!</h1>
         </div>
         <div className="absolute bottom-[-80px] right-0 z-0 w-[400px] md:w-[400px] lg:w-[500px]">
           <img
