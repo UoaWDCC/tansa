786b0063a5ed138acfaabd0d28b3c823ba9c5343
Tyler <tylerhy@proton.me>
2025-08-01 17:32:18 +1200

    insta feed

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './styles.css'
+import InstagramWidget from './components/InstagramWidget'
 
 export default async function HomePage() {
   return (
     <div>
       <div className="bg-tansa-blue h-[400px]"></div>
       <h1 className="">home page</h1>
+      <InstagramWidget />
       <div className="bg-tansa-cream h-[600px]"></div>
     </div>
   )
