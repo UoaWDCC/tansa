fd2a1e419c407eb1287a6b5523564b31adbe1842
cchiem <chris.chiem01@gmail.com>
2025-08-05 15:45:11 +1200

    fix: removed rich lexical editor and not found.tsx

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,5 +1,4 @@
 import { postgresAdapter } from '@payloadcms/db-postgres'
-import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
@@ -25,7 +24,6 @@ export default buildConfig({
     },
   },
   collections: [Users, Media, Events, NewsletterEmails, Sponsors, CSVUploads, Logos],
-  editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
