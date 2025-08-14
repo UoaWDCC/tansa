f6a706dc2a14586b0f26a1ab1137fa1b38661a36
cchiem <chris.chiem01@gmail.com>
2025-08-15 00:46:17 +1200

    feat: add header loading state

--- src/components/Skeleton.tsx
+++
@@ -1,7 +0,0 @@
-export default function SkeletonCard() {
-  return (
-    <div className="rounded-lg overflow-hidden bg-white shadow-md">
-      <div className="aspect-square relative overflow-hidden bg-gray-200 animate-pulse" />
-    </div>
-  )
-}
