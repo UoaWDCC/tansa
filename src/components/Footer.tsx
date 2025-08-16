928ee988a6dc33064e47ee86172664a6fc1b306b
deasylie <dlie042@aucklanduni.ac.nz>
2025-08-16 19:01:32 +1200

    Reformat footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -48,7 +48,7 @@ const Footer = () => {
   return (
     <footer className="bg-tansa-blue text-white p-6 mt-auto">
       <div className="max-w-6xl mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-5 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-6 gap-8 mb-4">
           {/* About Us Section */}
           <div className="md:col-span-1">
             <h3 className="font-bold text-xl mb-4">About Us</h3>
@@ -128,7 +128,7 @@ const Footer = () => {
           </div>
 
           {/* Newsletter Section */}
-          <div className="md:col-span-1">
+          <div className="md:col-span-2">
             {!subbed ? (
               <div className="">
                 <h3 className="font-bold text-xl mb-4">Subscribe to our newsletter!</h3>
