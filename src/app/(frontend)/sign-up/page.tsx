6062862e90648ec22023cf0d697000c150106b92
Tyler <tylerhy@proton.me>
2025-08-17 15:44:44 +1200

    Fixes.

--- src/app/(frontend)/sign-up/page.tsx
+++ src/app/(frontend)/sign-up/page.tsx
@@ -6,12 +6,13 @@ import React from 'react'
 const page = () => {
   return (
     <main className="min-h-screen select-none">
-      <div className="bg-tansa-blue relative h-[300px] flex items-center justify-center flex-col text-center space-y-4 rounded-b-4xl">
+      <div className="bg-tansa-blue relative h-[300px] flex items-center justify-center flex-col text-center space-y-4 rounded-b-4x overflow-clip">
         <h1 className="text-6xl md:text-8xl font-bold text-white font-newkansas z-10">Join Us</h1>
         <p className="text-lg md:text-2xl text-white z-10">
-          Join TANSA this year and get access to exclusive events, deals, and a vibrant community!
+          Join TANSA this year and get access to
+          <br /> exclusive events, deals, and a vibrant community!
         </p>
-         {/* Bear image */}
+        {/* Bear image */}
         <div className="absolute right-0 bottom-[-190px] select-none">
           <Image
             src="/bears/hooray 1.svg"
--- src/components/registration-form/Required.tsx
+++ src/components/registration-form/Required.tsx
@@ -1,7 +1,7 @@
 const Required = () => {
   return (
     <span className="text-white py-0.5 px-1 bg-tansa-blue text-xs rounded text-center items-center">
-      必填
+      required
     </span>
   )
 }
