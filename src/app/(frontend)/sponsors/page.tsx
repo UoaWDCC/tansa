00b9ff1feb62a99a5b408c424fa0bee53ce46e81
alanna <alannasantoso22@gmail.com>
2025-09-26 17:10:18 +1200

    chore: cleaning

--- src/app/(frontend)/sponsors/page.tsx
+++ src/app/(frontend)/sponsors/page.tsx
@@ -11,25 +11,19 @@ export default async function SponsorsPage() {
     <div>
       <div className="bg-tansa-blue">
         {/* Header Section */}
-        <div className="p-20 max-w-6xl h-[300px] mx-auto flex items-center justify-between pt-16 relative overflow-clip">
-          <div className="font-newkansas font-bold text-white leading-none  text-7xl md:text-8xl">
+        <div className="max-w-6xl h-[300px] mx-auto flex items-center justify-between pt-16 relative overflow-clip">
+          <div className="font-newkansas font-bold text-white leading-none text-8xl">
             <h1>Our</h1>
             <h1>Sponsors</h1>
           </div>
 
-          <div className="
-    absolute
-    right-0 bottom-0                /* default: keep it visible on small screens */
-    sm:right-[-10px] sm:bottom-[-100px]
-    md:right-[-20px] md:bottom-[-150px]
-    w-[180px] sm:w-[250px] md:w-[350px] 3fdft
-  ">
+          <div className="absolute right-[-20px] bottom-[-250px]">
             <Image
               src="/bears/running-pointing.svg"
               alt="Bear"
               width={450}
               height={450}
-              className="object-contain w-full h-auto"
+              className="object-contain"
             />
           </div>
         </div>
@@ -39,7 +33,7 @@ export default async function SponsorsPage() {
       <SponsorsList sponsors={sponsors} />
 
       {/* Map Section */}
-      <div className="flex justify-center p-20 m-15 mx-auto">
+      <div className="flex justify-center m-15 mx-auto">
         <iframe
           src="https://www.google.com/maps/d/embed?mid=1xX0X1w1pNLM0xoZjKMIOh_6y0CxNEiY&ehbc=2E312F"
           width="640"
